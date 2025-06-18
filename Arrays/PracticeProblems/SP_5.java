789a72766d3afce29852f0d639dc23f28ad7ae25
Subodh-1370 <subodhsingh1370@gmail.com>
2025-06-18 13:09:31 +0530

    [Subodh Singh]Add Arrays for Day4

--- Arrays/PracticeProblems/SP_5.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class CKTM{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        float Kilometers = sc.nextInt();
-        double Miles = Kilometers * 0.621371;
-        System.out.println(Miles);
-    }
-}

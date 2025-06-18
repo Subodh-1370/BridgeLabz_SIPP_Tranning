ecdc615aa69090a433014a367b1a0644ef7bf1c2
Subodh-1370 <subodhsingh1370@gmail.com>
2025-06-18 13:12:15 +0530

    [Subodh Singh]Add ControlFlow for Day3

---
+++ ControlFlow/Q_14.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Q_14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n==0){
+            System.out.println("The number is Zero");
+        }else if(n<0){
+            System.out.println("The number is less then Zero");
+        }else{
+            System.out.println("The number is greater then Zero");
+        }
+    }
+}
---
+++ ControlFlow/Q_6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Q_6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n==0){
+            System.out.println("Not a natural Number");
+        }
+        int i = 1;
+        while(i<n){
+            System.out.println(i);
+            i++;
+        }
+        int Sum = n*(n+1)/2;
+        System.out.println(Sum);
+    }
+}

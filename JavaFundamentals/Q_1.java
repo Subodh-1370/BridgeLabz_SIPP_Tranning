79629a3846a343f7fb1f5b3bce2765bc6a41abb8
Subodh-1370 <subodhsingh1370@gmail.com>
2025-06-23 12:12:44 +0530

    initial Commit on Empty OOPS branch

--- JavaFundamentals/Q_1.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class HarryAge{
-     public static void main(String[] args){
-         int CurrentYear = 2024;
-         int BirthYear = 2000;
-         int Age = CurrentYear - BirthYear ;
-         System.out.println(Age);
-     }
-}
--- PracticeProblems/AP_4.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class AreaOfCircle {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int radius = sc.nextInt();
-        float Area = 22/7 * radius^2;
-        System.out.println(Area);
-    }
-}

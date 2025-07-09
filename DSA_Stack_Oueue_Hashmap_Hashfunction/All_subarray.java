a564d3eba372bca47c5add485c51945f6894e6a0
Subodh-1370 <subodhsingh1370@gmail.com>
2025-07-09 10:35:39 +0530

    [Subodh Singh] add StackQueueHash for Day 15

--- DSA_Stack_Oueue_Hashmap_Hashfunction/All_subarray.java
+++ DSA_Stack_Oueue_Hashmap_Hashfunction/All_subarray.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class All_subarray {
+import java.util.Stack;
+
+public class All_subarray {
+    private Stack<Integer> s1;
+    private Stack<Integer> s2;
+
+    public All_subarray() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+
+    public void enqueue(int x) {
+        s1.push(x);
+    }
+
+    public int dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.peek();
+    }
+
+    public boolean isEmpty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        All_subarray q = new All_subarray();
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+        System.out.println("Dequeued: " + q.dequeue());
+        System.out.println("Front: " + q.peek());
+        System.out.println("Dequeued: " + q.dequeue());
+        System.out.println("Dequeued: " + q.dequeue());
+        System.out.println("Is empty? " + q.isEmpty());
+    }
 }
--- DSA_Stack_Oueue_Hashmap_Hashfunction/Check_for_pair.java
+++ DSA_Stack_Oueue_Hashmap_Hashfunction/Check_for_pair.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Check_for_pair {
+import java.util.HashSet;
+
+public class Check_for_pair {
+    public static boolean hasPairWithSum(int[] arr, int target) {
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : arr) {
+            if (seen.contains(target - num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 4, 1, 6};
+        int target = 10;
+
+        if (hasPairWithSum(arr, target)) {
+            System.out.println("Pair exists");
+        } else {
+            System.out.println("No such pair exists");
+        }
+    }
 }
